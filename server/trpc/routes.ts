6e870b46c27528fc5bc0f3eb0f19c2d555bbc989
sigma <seiya.sugimoto.free@gmail.com>
2023-08-16 23:34:24 +0900

    add new appRouter

--- server/trpc/routes.ts
+++ server/trpc/routes.ts
@@ -1,14 +1,6 @@
-import { initTRPC } from '@trpc/server'
-import { z } from 'zod'
+import { hello } from './hello'
+import { router } from '.'
 
-export const t = initTRPC.create()
-
-export const appRouter = t.router({
-  hello: t.procedure.input(z.object({ text: z.string().nullish()}).nullish()).query(({ input }) => {
-    return {
-      greeting: `Hello ${input?.text ?? 'World'}`,
-    }
-  }),
-})
+export const appRouter = router({ hello })
 
 export type AppRouter = typeof appRouter
