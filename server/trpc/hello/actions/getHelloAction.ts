89f46ce491dc631572561b3557ef2d8d39c11c1a
sigma <seiya.sugimoto.free@gmail.com>
2023-08-16 23:33:16 +0900

    add getHello action

---
+++ server/trpc/hello/actions/getHelloAction.ts
@@ -0,0 +1,8 @@
+import { z } from "zod"
+import { publicProcedure } from "../.."
+
+export const getHello = publicProcedure.input(z.object({ text: z.string().nullish()}).nullish()).query(({ input }) => {
+  return {
+    greeting: `Hello ${input?.text ?? 'World'}`,
+  }
+})
