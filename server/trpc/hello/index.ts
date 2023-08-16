f3b6a266033c157c5e647f7883ec00bdafb86aab
sigma <seiya.sugimoto.free@gmail.com>
2023-08-16 23:33:39 +0900

    add hello controller

---
+++ server/trpc/hello/index.ts
@@ -0,0 +1,8 @@
+import { router } from "..";
+import { getHello } from "./actions/getHelloAction";
+import { getHey } from "./actions/getHeyAction";
+
+export const hello = router({
+  getHello,
+  getHey
+})
