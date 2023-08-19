da37273eff7743adfceff9ff7f7c60cff9d80233
sigma <seiya.sugimoto.free@gmail.com>
2023-08-19 22:07:12 +0900

    add delete user service

---
+++ server/trpc/user/services/deleteUserService.ts
@@ -0,0 +1,29 @@
+import { Prisma, User } from "@prisma/client"
+import { ApplicationServiceError, ApplicationServiceResponse, prisma } from "../.."
+import { DeleteUser } from "../actions/deleteUserAction"
+
+export const deleteUserService = async (deleteUser: DeleteUser): Promise<ApplicationServiceResponse<User>> => {
+  try {
+    const deleted = await prisma.user.delete({
+      where: {
+        id: deleteUser.id
+      }
+    })
+    return {
+      data: deleted,
+      error: null
+    }
+  } catch (error) {
+    console.error(error)
+    if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2025') {
+      return {
+        data: null,
+        error: new ApplicationServiceError('user not found', 'NOT_FOUND')
+      }
+    }
+    return {
+      data: null,
+      error: new ApplicationServiceError('failed prisma.user.delete', 'INTERNAL_SERVER_ERROR')
+    }
+  }
+}
