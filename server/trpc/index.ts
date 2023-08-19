c0e88314cba0b749c064b622af2c1bea4ff9f629
sigma <seiya.sugimoto.free@gmail.com>
2023-08-19 22:06:09 +0900

    add prisma client and application service response

--- server/trpc/index.ts
+++ server/trpc/index.ts
@@ -1,7 +1,27 @@
 import { initTRPC } from '@trpc/server'
+import { PrismaClient } from '@prisma/client'
+import { TRPC_ERROR_CODE_KEY } from '@trpc/server/rpc'
 
 export const t = initTRPC.create()
 
 export const middleware = t.middleware
 export const router = t.router
 export const publicProcedure = t.procedure
+
+export const prisma = new PrismaClient()
+
+export type ApplicationServiceErrorCode = TRPC_ERROR_CODE_KEY
+
+export class ApplicationServiceError extends Error {
+  constructor(message: string, public code: ApplicationServiceErrorCode) {
+    super(message)
+  }
+}
+
+export type ApplicationServiceResponse<T> = {
+  data: T
+  error: null
+} | {
+  data: null
+  error: ApplicationServiceError
+}
