c3c51008d0fed955916ef127b803d6d3f591fd19
sigma <seiya.sugimoto.free@gmail.com>
2023-08-19 22:07:54 +0900

    add user router to appRouter

--- server/trpc/routes.ts
+++ server/trpc/routes.ts
@@ -1,6 +1,7 @@
 import { hello } from './hello'
+import { user } from './user'
 import { router } from '.'
 
-export const appRouter = router({ hello })
+export const appRouter = router({ hello, user })
 
 export type AppRouter = typeof appRouter
