e354be4e54c73b240dd856c3bfefab3af21c25d2
sigma <seiya.sugimoto.free@gmail.com>
2023-08-21 06:30:27 +0900

    add seeder

---
+++ src/prisma/seed.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+  await prisma.user.upsert({
+    where: { email: 'alice@prisma.io' },
+    update: {},
+    create: {
+      email: 'alice@prisma.io',
+      name: 'Alice',
+      password: 'password'
+    },
+  })
+  await prisma.user.upsert({
+    where: { email: 'bob@prisma.io' },
+    update: {},
+    create: {
+      email: 'bob@prisma.io',
+      name: 'Bob',
+      password: 'password'
+    },
+  })
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
