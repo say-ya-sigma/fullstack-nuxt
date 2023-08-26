80c6c6f49f35f7121a36d2c654dfc01d86585815
sigma <seiya.sugimoto.free@gmail.com>
2023-08-26 16:29:34 +0900

    remove index both

--- nuxt/server/trpc/index.ts
+++ nuxt/server/trpc/index.ts
@@ -1,6 +1,5 @@
 import { initTRPC } from '@trpc/server'
 import { PrismaClient } from '@prisma/client'
-import { TRPC_ERROR_CODE_KEY } from '@trpc/server/rpc'
 
 export const t = initTRPC.create()
 
@@ -9,19 +8,3 @@ export const router = t.router
 export const publicProcedure = t.procedure
 
 export const prisma = new PrismaClient()
-
-export type ApplicationServiceErrorCode = TRPC_ERROR_CODE_KEY
-
-export class ApplicationServiceError extends Error {
-  constructor(message: string, public code: ApplicationServiceErrorCode) {
-    super(message)
-  }
-}
-
-export type ApplicationServiceResponse<T> = {
-  data: T
-  error: null
-} | {
-  data: null
-  error: ApplicationServiceError
-}
